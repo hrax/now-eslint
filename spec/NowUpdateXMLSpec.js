1097c711a7a440d8d4f57fc5a76d5fbdc86c55fe
Gregor "hrax" Magdolen <magdolen@elepha.info>
2020-08-04 03:49:46 +0200

    Payload parsing tests

--- spec/NowUpdateXMLSpec.js
+++ spec/NowUpdateXMLSpec.js
@@ -1,4 +1,5 @@
 const NowUpdate = require("../src/NowUpdateXML.js");
+const fs = require("fs");
 
 describe("NowUpdateXML", () => {
   it("throws error on incorrect data config.", () => {
@@ -47,12 +48,60 @@ describe("NowUpdateXML", () => {
     expect(updateXML.status).toBe(NowUpdate.NowUpdateXMLStatus.IGNORE);
   });
 
-  xit("properly parses generic XML payload from the JSON", () => {
+  // Must be a XML with root element "record_update" and attribute table containing table name with child element having the same value
+  it("properly parses generic XML payload from the JSON", () => {
+    // Path expects the test to be executed from project root via npm test
+    const payload = fs.readFileSync("./spec/payloads/sys_script_include_50ba882f07d610108110f2ae7c1ed00d.xml", {encoding:'utf8'});
+    const data = {
+      "name": "sys_script_include_50ba882f07d610108110f2ae7c1ed00d",
+      "sys_id": "43ca802f07d610108110f2ae7c1ed05a",
+      "action": NowUpdate.NowUpdateXMLAction.INSERT_OR_UPDATE,
+      "sys_created_by": "admin",
+      "sys_created_on": "1970-01-01 00:00:01",
+      "sys_updated_by": "admin",
+      "sys_updated_on": "1970-01-01 00:00:01",
+      "type": "Script Include",
+      "target_name": "sys_script_include_50ba882f07d610108110f2ae7c1ed00d",
+      "update_set": "1234",
+      "payload": payload
+    };
 
+    const updateXML = new NowUpdate.NowUpdateXML(data, false);
+
+    expect(updateXML.payload).toBeDefined();
+    expect(updateXML.payload["script"]).toBeDefined();
+    expect(updateXML.payload["script"]._cdata).toBeDefined();
+    expect(updateXML.payload["script"]._cdata).toMatch("TestGlass");
   });
 
-  xit("sets field report", () => {
+  it("sets field report", () => {
+    const data = {
+      "name": "",
+      "sys_id": "",
+      "action": NowUpdate.NowUpdateXMLAction.INSERT_OR_UPDATE,
+      "sys_created_by": "",
+      "sys_created_on": "",
+      "sys_updated_by": "",
+      "sys_updated_on": "",
+      "type": "",
+      "target_name": "",
+      "update_set": "",
+      "payload": ""
+    };
 
+    const report = {
+      "warningCount": 1,
+      "errorCount": 2
+    };
+
+    const updateXML = new NowUpdate.NowUpdateXML(data, true);
+    updateXML.setReport("field", report);
+
+    expect(updateXML.hasReports).toBe(true);
+    expect(updateXML.reports).toEqual([report]);
+    expect(updateXML.reportFields).toEqual(["field"]);
+    expect(updateXML.warningCount).toBe(1);
+    expect(updateXML.errorCount).toBe(2);
   });
 
   xdescribe("Non-standard XML payload parsing", () => {
