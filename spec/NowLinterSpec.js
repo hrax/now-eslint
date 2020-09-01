c80ac97367c9c1193e96c3f1be5581d94ee1e4f6
Gregor "hrax" Magdolen <magdolen@elepha.info>
2020-09-02 01:37:19 +0200

    Empty test field in payload  and NowLinter tests

--- spec/NowLinterSpec.js
+++ spec/NowLinterSpec.js
@@ -12,20 +12,25 @@ describe("NowLinter", () => {
     }, options, tables);
   };
 
-  const payload = fs.readFileSync("./spec/payloads/sys_script_include_50ba882f07d610108110f2ae7c1ed00d.xml", {encoding: "utf8"});
-  const data = {
-    "name": "sys_script_include_50ba882f07d610108110f2ae7c1ed00d",
-    "sys_id": "43ca802f07d610108110f2ae7c1ed05a",
-    "action": NowUpdateXMLAction.INSERT_OR_UPDATE,
-    "sys_created_by": "admin",
-    "sys_created_on": "1970-01-01 00:00:01",
-    "sys_updated_by": "admin",
-    "sys_updated_on": "1970-01-01 00:00:01",
-    "type": "Script Include",
-    "target_name": "sys_script_include_50ba882f07d610108110f2ae7c1ed00d",
-    "update_set": "1234",
-    "payload": payload
-  };
+  let payload = null;
+  let data = null;
+
+  beforeEach(() => {
+    payload = fs.readFileSync("./spec/payloads/sys_script_include_50ba882f07d610108110f2ae7c1ed00d.xml", {encoding: "utf8"});
+    data = {
+      "name": "sys_script_include_50ba882f07d610108110f2ae7c1ed00d",
+      "sys_id": "43ca802f07d610108110f2ae7c1ed05a",
+      "action": NowUpdateXMLAction.INSERT_OR_UPDATE,
+      "sys_created_by": "admin",
+      "sys_created_on": "1970-01-01 00:00:01",
+      "sys_updated_by": "admin",
+      "sys_updated_on": "1970-01-01 00:00:01",
+      "type": "Script Include",
+      "target_name": "sys_script_include_50ba882f07d610108110f2ae7c1ed00d",
+      "update_set": "1234",
+      "payload": payload
+    };
+  });
 
   it("Fetch and prepare NowUpdateXML records", async() => {
     const options = {
@@ -50,9 +55,9 @@ describe("NowLinter", () => {
       "title": "Test Report",
       "query": "name=Default"
     };
+    const linter = _createLinter(options, {"sys_script_include": ["script"]});
     
     // Not testing NowLoader
-    const linter = _createLinter(options, {"sys_script_include": ["script"]});
     linter.getLoader();
     spyOn(linter.loader, "fetchUpdateXMLByUpdateSetQuery").and.returnValue({result: [data]});
 
@@ -72,5 +77,64 @@ describe("NowLinter", () => {
     expect(changes.length).toBe(1);
     expect(changes[0].status).toBe(NowUpdateXMLStatus.OK);
     expect(changes[0].reportEntries[0][0]).toBe("script");
+  });
+
+  it("Ignore not configured tables", async () => {
+    const options = {
+      "title": "Test Report",
+      "query": "name=Default"
+    };
+    
+    const linter = _createLinter(options, {"sys_script": ["script"]});
+    
+    // Not testing NowLoader
+    linter.getLoader();
+    spyOn(linter.loader, "fetchUpdateXMLByUpdateSetQuery").and.returnValue({result: [data]});
+
+     // Not testing ESLint
+    linter.getESLintCLI();
+    spyOn(linter.cli, "executeOnText").and.callThrough();
+
+    await linter.process();
+
+    const changes = linter.getChanges();
+
+    expect(linter.loader.fetchUpdateXMLByUpdateSetQuery).toHaveBeenCalledTimes(1);
+    expect(linter.cli.executeOnText).not.toHaveBeenCalled();
+
+    expect(changes.length).toBe(1);
+    expect(changes[0].status).toBe(NowUpdateXMLStatus.IGNORE);
+    expect(changes[0].hasReports).toBe(false);
+  });
+
+  it("Skip on empty field", async () => {
+    payload = fs.readFileSync("./spec/payloads/sys_script_include_empty_script.xml", {encoding: "utf8"});
+    data.payload = payload;
+
+    const options = {
+      "title": "Test Report",
+      "query": "name=Default"
+    };
+    
+    const linter = _createLinter(options, {"sys_script_include": ["script"]});
+    
+    // Not testing NowLoader
+    linter.getLoader();
+    spyOn(linter.loader, "fetchUpdateXMLByUpdateSetQuery").and.returnValue({result: [data]});
+
+     // Not testing ESLint
+    linter.getESLintCLI();
+    spyOn(linter.cli, "executeOnText").and.callThrough();
+
+    await linter.process();
+
+    const changes = linter.getChanges();
+
+    expect(linter.loader.fetchUpdateXMLByUpdateSetQuery).toHaveBeenCalledTimes(1);
+    expect(linter.cli.executeOnText).not.toHaveBeenCalled();
+
+    expect(changes.length).toBe(1);
+    expect(changes[0].status).toBe(NowUpdateXMLStatus.SKIPPED);
+    expect(changes[0].hasReports).toBe(false);
   });
 });
