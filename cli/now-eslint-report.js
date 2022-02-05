9ae67ec76d108867e826e07124eb684a49d45620
Gregor "hrax" Magdolen <magdolen@elepha.info>
2022-02-05 05:21:12 +0100

    Commander generate PDF report command

--- cli/now-eslint-report.js
+++ cli/now-eslint-report.js
@@ -24,80 +24,100 @@ const PROFILE_HOME = Profile.profilesHomeDirPath();
 prompt.message = "";
 prompt.delimiter = "";
 
+/**
+ * Validates if value matches profile name pattern
+ * @param {String} value 
+ * @returns value if value matches profile name pattern
+ * @throws commander.InvalidArgumentError if value does not match
+ */
+const validateProfileName = (value) => {
+  if (Profile.isProfileNameValid(value)) {
+    return value;
+  }
+  throw new commander.InvalidArgumentError("'name' can only contain lowecase/uppercase letters, numbers, underscore and dash.");
+};
+
+const validateFileName = (value) => {
+  if (Profile.isProfileNameValid(value)) {
+    return value;
+  }
+  throw new commander.InvalidArgumentError("'file-name' can only contain lowecase/uppercase letters, numbers, underscore and dash.");
+};
+
+const debugWorkingDir = () => {
+  console.debug("Working profiles home directory: " + colors.green(`'${PROFILE_HOME}'\n`));
+};
+
 program.name("now-eslint report")
   .description("CLI to generate update set report from ServiceNow instance")
   .configureOutput({outputError: (str, write) => write(colors.red(str))})
-  .argument("<profileName>")
-  .option("-t, --title", "title of the report (in quotes for multiword titles)")
-  .option("-f, --file-name", "file name of the report without extension; if not provided one will be generated from title")
-  .option("-q, --query", "update set query to perform report on")
+  .argument("<name>", "name of the profile to set up (lowecase/uppercase letters, numbers, underscore and dash)", validateProfileName)
+  .option("-t, --title <name>", "title of the report (in quotes for multiword titles)")
+  .option("-f, --file-name <name>", "file name of the report without an extension", validateFileName)
+  .option("-q, --query <query>", "update set query to perform report on")
   // .option("--json", "generate report as JSON rather than PDF report")
   // .option("--with-json", "generate JSON for the PDF report; ignored if option --json is used")
-  .action(async(profileName, options) => {
+  .action(async(name, options) => {
+    debugWorkingDir();
+
+    if (!Profile.exists(name)) {
+      program.error(`Profile with name '${name}' does not exists`, {exitCode: 1});
+    }
+
     const schema = {
       properties: {
-        profile: {
-          description: colors.yellow("Enter the name of the profile to use"),
-          required: true,
-          message: colors.red("Given profile does not exists"),
-          conform: (value) => {
-            return fs.existsSync(`${PROFILE_HOME}/profile_${value}/profile.json`);
-          }
-        },
         title: {
           description: colors.yellow("Enter the name of your report:"),
-          required: true
+          required: true,
+          ask: () => {
+            return options.title == null;
+          }
         },
-        filename: {
+        fileName: {
           description: colors.yellow("Enter the file name of your report (without an extension):"),
           pattern: /^[a-zA-Z0-9\-_]+$/,
           message: colors.red("File name must contain only lower case letters, numbers and dash (-) or underscore (_)"),
-          required: true
-          // .replace(/([^a-z0-9_-])/gi, "_").replace(/_{2,}/gi, "_").replace(/^_|_$/gi, "");
+          ask: () => {
+            return options.fileName == null;
+          }
         },
         query: {
           description: colors.yellow("Enter the query of the report:"),
-          required: true
+          required: true,
+          ask: () => {
+            return options.query == null;
+          }
         }
       }
     };
+
+    prompt.start();
+
+    prompt.get(schema, async function(err, result) {
+      if (err) {
+        program.error(err);
+      }
+
+      const fileName = options.fileName || result.fileName;
+      const profile = Profile.load(name);
+      const opt = {};
+      opt.query = options.query || result.query;
+      opt.title = options.title || result.title;
+
+      const linter = new NowLinter(profile, opt);
+      
+      console.info(colors.green(`Fetching data from the instance`));
+      await linter.fetch();
+
+      console.info(colors.green(`Linting fetched data`));
+      await linter.lint();
+      
+      
+      console.info(colors.green(`Generating report '${fileName}.pdf'`));
+      linter.report(`${process.cwd()}/${fileName}.pdf`);
+
+      console.info(colors.green(`Saved report '${fileName}.pdf'`));
+    });
   });
 
-return;
-
-
-prompt.start();
-
-prompt.get(schema, (err, result) => {
-  if (err) {
-    return;
-  }
-
-  const data = fs.readFileSync(`${PROFILE_HOME}/profile_${result.profile}/profile.json`, "utf8");
-  const profile = new Profile(JSON.parse(data));
-  
-  const options = {};
-  options.query = result.query;
-  options.title = result.title;
-
-  /*
-  TODO:
-  - If eslint config loads plugins from special directory, show warning
-  - If eslint config overrides config file, show warning
-   */
-
-  const linter = new NowLinter(profile, options);
-  (async function() {
-    console.log(colors.green(`Fetching data from the instance`));
-    await linter.fetch();
-
-    console.log(colors.green(`Linting fetched data`));
-    await linter.lint();
-    
-    console.log(colors.green(`Generating report '${result.filename}'`));
-    const setup = require("../resources/pdfsetup");
-    
-    console.log(colors.green(`Saving report '${result.filename}.pdf'`));
-    linter.report(`${process.cwd()}/${result.filename}.pdf`, setup)
-  })();
-});
+program.parseAsync(process.argv);
