70eb090969840cc47d368cb353d5f3263b167445
Gregor "hrax" Magdolen <magdolen@elepha.info>
2022-02-16 02:21:33 +0100

    JSONReportGenerator tests

--- spec/generatorSpec/JSONReportGeneratorSpec.js
+++ spec/generatorSpec/JSONReportGeneratorSpec.js
@@ -1,5 +1,7 @@
 const JSONReportGenerator = require("../../src/generator/JSONReportGenerator");
 
+const UpdateXMLScan = require("../../src/UpdateXMLScan");
+
 describe("JSONReportGenerator", () => {
   let data = {}
 
@@ -9,10 +11,80 @@ describe("JSONReportGenerator", () => {
       username: "username",
       title: "title",
       query: "query",
-      changes: [],
-      resources: {}
+      changes: [
+        ["change1", 
+          {
+            targetName: "targetName",
+            type: "type",
+            id: "id",
+            createdBy: "createdBy",
+            createdOn: "createdOn",
+            updatedBy: "updatedBy",
+            updatedOn: "updatedOn",
+            status: UpdateXMLScan.STATUS.ERROR,
+            action: "INSERT_OR_UPDATE",
+            updates: "1",
+            errorCount: "1",
+            warningCount: "0",
+            updateSet: "updateSet",
+            targetTable: "targetTable",
+            targetId: "targetId",
+            reports: [
+              ["script", {
+                messages: [
+                  {
+                    line: 1,
+                    column: 1,
+                    severity: 2,
+                    ruleId: "no-console",
+                    message: "message"
+                  }
+                ]
+              }]
+            ]
+          }
+        ],
+        ["change2", 
+          {
+            targetName: "targetName",
+            type: "type",
+            id: "id",
+            createdBy: "createdBy",
+            createdOn: "createdOn",
+            updatedBy: "updatedBy",
+            updatedOn: "updatedOn",
+            status: UpdateXMLScan.STATUS.WARNING,
+            action: "INSERT_OR_UPDATE",
+            updates: "1",
+            errorCount: "0",
+            warningCount: "1",
+            updateSet: "updateSet",
+            targetTable: "targetTable",
+            targetId: "targetId",
+            reports: [
+              ["script", {
+                messages: [
+                  {
+                    line: 1,
+                    column: 1,
+                    severity: 1,
+                    ruleId: "no-console",
+                    message: "message"
+                  }
+                ]
+              }]
+            ]
+          }
+        ]
+      ],
+      resources: {
+        "overview-resources": [{
+          label: "Google",
+          link: "http://example.com"
+        }]
+      }
     };
-  })
+  });
 
   it("builds document body", () => {
     const generator = new JSONReportGenerator();
