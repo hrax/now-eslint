9e4379eba0a02ac40a8846f7b37c1ab41a40677f
Gregor "hrax" Magdolen <magdolen@elepha.info>
2022-02-18 05:06:16 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 module.exports = {
-  NOW: require("./src/now/now"),
-  AbstractReportGenerator: require("./src/generator/AbstractReportGenerator"),
-  PDFReportGenerator: require("./src/generator/PDFReportGenerator"),
-  JSONReportGenerator: require("./src/generator/JSONReportGenerator"),
   Profile: require("./src/Profile"),
-  NowLinter: require("./src/NowLinter"),
-  UpdateXMLScan: require("./src/UpdateXMLScan")
+  NowLinter: require("./src/NowLinter")
 };
